5221bd7bb2dc7a5f132d02fc225c1a0be669ccf6
coach paco <paco@csedge.org>
2022-05-26 10:29:02 -0700

    added button and action for play/pause

--- videoTestApp/ViewController.swift
+++ videoTestApp/ViewController.swift
@@ -10,6 +10,9 @@ import AVKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var isPlaying = false
+    var player = AVPlayer(playerItem: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,9 +22,10 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
        
         
-        let videoURL: NSURL = Bundle.main.url(forResource: "video_1", withExtension: "mp4")! as NSURL
+        let videoURL: NSURL = Bundle.main.url(forResource: "video_2", withExtension: "mp4")! as NSURL
         
-        let player = AVPlayer(url: videoURL as URL)
+       // let player = AVPlayer(url: videoURL as URL)
+        self.player.replaceCurrentItem(with: AVPlayerItem(url: videoURL as URL))
         
         //object to render player to view: AVPlayerLayer -- gets instatiated with a player
         let playerLayer = AVPlayerLayer(player: player)
@@ -36,12 +40,43 @@ class ViewController: UIViewController {
         view.layer.addSublayer(playerLayer)
         
         //begin playing
-        player.play()
+        //self.player.pause()
+        
+        
+        //button does nothing yet, just practicing formatting it
+        let playStopBtn = UIButton(type: .system)
+        
+       
+        //playStopBtn.setTitle("Stop", for: .normal)
+        playStopBtn.setBackgroundImage(UIImage(systemName: "play.circle"), for: .normal)
+       
+        playStopBtn.layer.cornerRadius = 48
+        playStopBtn.clipsToBounds = true
+        
+         
+        playStopBtn.center = view.center
+        playStopBtn.frame = CGRect(x: view.center.x-50, y: view.frame.height - 180, width: 100, height: 100)
+        view.addSubview(playStopBtn)
+        
+        
+        playStopBtn.addAction(UIAction(handler: { _ in
+            
+            if self.isPlaying{
+                self.isPlaying = false
+                self.player.pause()
+                
+            } else {
+                self.isPlaying = true
+                self.player.play()
+            }
+            print("Toggle Play: " + String(self.isPlaying))
+            
+        }), for: .touchUpInside)
         
         
         
     }
-
-
+    
+ 
 }
 
