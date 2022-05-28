585f010041fd5bc07252cd407ec6c5a28dc3b7aa
coach paco <paco@csedge.org>
2022-05-28 13:24:04 -0700

    Added looper and pause button

--- videoTestApp/ViewController.swift
+++ videoTestApp/ViewController.swift
@@ -2,7 +2,8 @@
 //  ViewController.swift
 //  videoTestApp
 //
-//  Created by admin on 5/25/22.
+//
+// Looper example
 //
 
 import UIKit
@@ -10,6 +11,11 @@ import AVKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    //AVPlayerLooper is used instead of AVPlayer
+    var playerLooper: AVPlayerLooper!
+    var queuePlayer: AVQueuePlayer!
+    var playStopBtn = UIButton(type: .system)
     
     var isPlaying = false
     var player = AVPlayer(playerItem: nil)
@@ -24,14 +30,19 @@ class ViewController: UIViewController {
         
         let videoURL: NSURL = Bundle.main.url(forResource: "video_2", withExtension: "mp4")! as NSURL
         
-       // let player = AVPlayer(url: videoURL as URL)
-        self.player.replaceCurrentItem(with: AVPlayerItem(url: videoURL as URL))
+        let asset: AVAsset = AVAsset.init(url: videoURL as URL)
+        
+        let playerItem = AVPlayerItem(asset: asset)
+        
+        self.queuePlayer = AVQueuePlayer(playerItem: playerItem)
+        
+        self.playerLooper = AVPlayerLooper(player: queuePlayer, templateItem: playerItem)
         
         //object to render player to view: AVPlayerLayer -- gets instatiated with a player
-        let playerLayer = AVPlayerLayer(player: player)
+        let playerLayer = AVPlayerLayer(player: queuePlayer)
         
         //set the player frame to fit the whole screen
-        playerLayer.frame = view.bounds
+         playerLayer.frame = view.bounds
         
         // optional videoGravity fills screen without distorting the aspect ration
         playerLayer.videoGravity = .resizeAspectFill
@@ -39,44 +50,35 @@ class ViewController: UIViewController {
         //as as a sublayer
         view.layer.addSublayer(playerLayer)
         
-        //begin playing
-        //self.player.pause()
-        
-        
-        //button does nothing yet, just practicing formatting it
-        let playStopBtn = UIButton(type: .system)
-        
-       
         //playStopBtn.setTitle("Stop", for: .normal)
-        playStopBtn.setBackgroundImage(UIImage(systemName: "play.circle"), for: .normal)
+        self.playStopBtn.setBackgroundImage(UIImage(systemName: "play.circle"), for: .normal)
        
-        playStopBtn.layer.cornerRadius = 48
-        playStopBtn.clipsToBounds = true
+        self.playStopBtn.layer.cornerRadius = 48
+        self.playStopBtn.clipsToBounds = true
         
          
-        playStopBtn.center = view.center
-        playStopBtn.frame = CGRect(x: view.center.x-50, y: view.frame.height - 180, width: 100, height: 100)
+        self.playStopBtn.center = view.center
+        self.playStopBtn.frame = CGRect(x: view.center.x-50, y: view.frame.height - 180, width: 100, height: 100)
         view.addSubview(playStopBtn)
         
-        
-        playStopBtn.addAction(UIAction(handler: { _ in
+        self.playStopBtn.addAction(UIAction(handler: { _ in
             
             if self.isPlaying{
                 self.isPlaying = false
-                self.player.pause()
+                self.queuePlayer.pause()
+                self.playStopBtn.setBackgroundImage(UIImage(systemName: "play.circle"), for: .normal)
                 
             } else {
                 self.isPlaying = true
-                self.player.play()
+                self.queuePlayer.play()
+                self.playStopBtn.setBackgroundImage(UIImage(systemName: "pause.circle"), for: .normal)
+                
             }
             print("Toggle Play: " + String(self.isPlaying))
             
         }), for: .touchUpInside)
         
-        
-        
     }
     
- 
 }
 
